e210633599fa5552c05a80d5e35dc3cb353cae41
Atsushi Ishida <aishidajt9@gmail.com>
2016-10-29 16:47:24 +0900

    Update SSP2015.R

--- SSP2015.R
+++ SSP2015.R
@@ -2,7 +2,10 @@
 ########## SSP2015 #########
 ############################
 
+##俺様用WD
+setwd("C:/Users/Atsushi_Ishida/Dropbox/My Work/000000【作業中バッファ】000000/SSP関連/SSP2015分析")
+
+
 SSP2015<-foreign::read.spss("SSPI2015_20150923_ver1.sav",use.value.labels=F,to.data.frame=T,use.missings = T)
 attach(SSP2015)
 
-detach(SSP2015

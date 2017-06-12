6d8b79fc3595333e9a4fdd344b1c1e52c993f7f9
DESKTOP-3DV3IHJ\z270 <hamada@m.tohoku.ac.jp>
2017-06-12 12:05:51 +0900

    コンフリクト解消

--- SSP2015.R
+++ SSP2015.R
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 #test by hamada
-=======
+
 ############################
 ########## SSP2015 #########
 ############################
 
 SSP2015<-foreign::read.spss("SSPI2015_20150923_ver1.sav",use.value.labels=F,to.data.frame=T,use.missings = T)
 attach(SSP2015)
->>>>>>> refs/remotes/aishidajt9/master
+
